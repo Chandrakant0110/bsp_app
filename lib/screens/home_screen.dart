871991dc528498c7f0802a549ff92833194d1d1c
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-06-28 12:43:03 +0530

    updated homepage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bsp_app/screens/pdf_screen.dart';
+import 'package:bsp_app/screens/services/services.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final Services services = Services();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,92 +54,146 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 10,
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Data-Driven Optimization of Efficiency and Delay Reduction in Work-in-Progress Jobs of MARS-I in Bhilai Steel Plant.',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+      body: RefreshIndicator(
+        onRefresh: () async {},
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Data-Driven Optimization of Efficiency and Delay Reduction in Work-in-Progress Jobs of MARS-I in Bhilai Steel Plant.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              const Divider(),
-              const Text(
-                'Our Mentees',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+                const Divider(),
+                const Text(
+                  'Our Mentees',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                const Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '- Mr. Rajeev Sontake, General Manager, CPD',
+                      ),
+                      Text(
+                        '- Mr. Ramesh Parganiha, Deputy General Manager, MARS-I',
+                      ),
+                      Text(
+                        '- Mr. A.P. Malla, Assistant General Manager, CPD',
+                      ),
+                      Text(
+                        '- Mr. Amulya Priyadarshini, Chief General Manager, HRD',
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                const Text(
+                  'Project Team',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '- Chandrakant Sahu',
+                      ),
+                      Text(
+                        '- Divyanshu Kumar',
+                      ),
+                      Text(
+                        '- Harsh Kumar Sonkar',
+                      ),
+                      Text(
+                        '- Jeevesh Banchhor',
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+                const Text(
+                  'Project Pdf\'s',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Text(
-                      '- Mr. Rajeev Sontake, General Manager, CPD',
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          PDFScreen.route(
+                            '',
+                            'assets/pdfs/Final_Report_BSP.pdf',
+                          ),
+                        );
+                      },
+                      child: const Text('Project Report'),
                     ),
-                    Text(
-                      '- Mr. Ramesh Parganiha, Deputy General Manager, MARS-I',
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          PDFScreen.route(
+                            '',
+                            'assets/pdfs/Impact_DataFinal.pdf',
+                          ),
+                        );
+                      },
+                      child: const Text('Model Pdf'),
                     ),
-                    Text(
-                      '- Mr. C.P. Malla, Assistant General Manager, CPD',
-                    ),
-                    Text(
-                      '- Mr. Amulya Priyadarshini, Chief General Manager, HRD',
+                    ElevatedButton(
+                      onPressed: () {
+                        services.launchURL(
+                            'https://colab.research.google.com/drive/1TyUm4DIaCTddXj0ZzdUm5xZbVMUfrkom?usp=sharing');
+                      },
+                      child: const Text('LAUNCHuRL'),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 6,
-              ),
-              const Text(
-                'Project Team',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+
+                const Divider(),
+                const Text(
+                  'Machines Available In MARS-I',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '- Chandrakant Sahu',
-                    ),
-                    Text(
-                      '- Divyanshu Kumar',
-                    ),
-                    Text(
-                      '- Harsh Kumar Sonkar',
-                    ),
-                    Text(
-                      '- Jeevesh Banchhor',
-                    ),
-                  ],
+                Image.asset('assets/images/machine_1.jpg'),
+                // Text('data'),
+                const SizedBox(
+                  height: 6,
                 ),
-              ),
-              const Divider(),
-              const Text(
-                'Machines Available In MARS-I',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+                Image.asset('assets/images/machine_2.jpg'),
+                // Text('data'),
+                const SizedBox(
+                  height: 6,
                 ),
-              ),
-              Image.asset('assets/images/machine_1.jpg'),
-              Text('data'),
-              Image.asset('assets/images/machine_2.jpg'),
-              Text('data'),
-              Image.asset('assets/images/machine_3.jpg'),
-            ],
+                Image.asset('assets/images/machine_3.jpg'),
+              ],
+            ),
           ),
         ),
       ),
